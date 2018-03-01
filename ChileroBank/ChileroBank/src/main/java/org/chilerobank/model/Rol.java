3c779051851dd8e274efabe2d9c15e3c8dccf58a
elmutzus <elmutzus@gmail.com>
2018-02-28 20:38:10 -0600

    First approach of adding models

--- ChileroBank/ChileroBank/src/main/java/org/chilerobank/model/Rol.java
+++ ChileroBank/ChileroBank/src/main/java/org/chilerobank/model/Rol.java
@@ -5,10 +5,81 @@
  */
 package org.chilerobank.model;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
 /**
  *
  * @author Elder Mutzus <elmutzus@gmail.com>
  */
-public class Rol {
-    
+@Entity
+@Table(name = "ROL")
+public class Rol implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "rolGen")
+    @SequenceGenerator(name = "rolGen", sequenceName = "rol_seq", initialValue = 10)
+    @Column(name = "ID")
+    private Integer id;
+
+    private String nombre;
+    private String descripcion;
+
+    public Rol() {
+    }
+
+    public Rol(Integer id, String nombre, String descripcion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
 }
--- ChileroBank/ChileroBank/src/main/java/org/chilerobank/model/Usuario.java
+++ ChileroBank/ChileroBank/src/main/java/org/chilerobank/model/Usuario.java
@@ -5,10 +5,222 @@
  */
 package org.chilerobank.model;
 
+import java.beans.Transient;
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 /**
  *
  * @author Elder Mutzus <elmutzus@gmail.com>
  */
-public class Usuario {
-    
+@Entity
+@Table(name = "USUARIO")
+public class Usuario implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "usuarioGen")
+    @SequenceGenerator(name = "usuarioGen", sequenceName = "usuario_seq", initialValue = 10)
+    private Integer id;
+
+    private String codigo;
+    private String email;
+
+    @Temporal(TemporalType.DATE)
+    private Date fechaNacimiento;
+
+    private String mimeType;
+    private String nombre;
+    private String nombreArchivo;
+    private String password;
+    private String telefono;
+
+    @Transient
+    private String fechaNacimientoConFormato;
+
+    @JoinColumn(name = "ID_ROL", referencedColumnName = "ID")
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private Rol rol;
+
+    public Usuario() {
+    }
+
+    public Usuario(Integer id, String codigo, String email, Date fechaNacimiento, String mimeType, String nombre, String nombreArchivo, String password, String telefono, String fechaNacimientoConFormato, Rol rol) {
+        this.id = id;
+        this.codigo = codigo;
+        this.email = email;
+        this.fechaNacimiento = fechaNacimiento;
+        this.mimeType = mimeType;
+        this.nombre = nombre;
+        this.nombreArchivo = nombreArchivo;
+        this.password = password;
+        this.telefono = telefono;
+        this.fechaNacimientoConFormato = fechaNacimientoConFormato;
+        this.rol = rol;
+    }
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @return the codigo
+     */
+    public String getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @return the fechaNacimiento
+     */
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    /**
+     * @return the mimeType
+     */
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @return the nombreArchivo
+     */
+    public String getNombreArchivo() {
+        return nombreArchivo;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @return the telefono
+     */
+    public String getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * @return the fechaNacimientoConFormato
+     */
+    public String getFechaNacimientoConFormato() {
+        return fechaNacimientoConFormato;
+    }
+
+    /**
+     * @return the rol
+     */
+    public Rol getRol() {
+        return rol;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @param fechaNacimiento the fechaNacimiento to set
+     */
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    /**
+     * @param mimeType the mimeType to set
+     */
+    public void setMimeType(String mimeType) {
+        this.mimeType = mimeType;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @param nombreArchivo the nombreArchivo to set
+     */
+    public void setNombreArchivo(String nombreArchivo) {
+        this.nombreArchivo = nombreArchivo;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @param telefono the telefono to set
+     */
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    /**
+     * @param fechaNacimientoConFormato the fechaNacimientoConFormato to set
+     */
+    public void setFechaNacimientoConFormato(String fechaNacimientoConFormato) {
+        this.fechaNacimientoConFormato = fechaNacimientoConFormato;
+    }
+
+    /**
+     * @param rol the rol to set
+     */
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
 }
